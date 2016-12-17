721c206ba315013c23d38f36cc1af2eeeb4cc292
Donald Pinckney <donald_pinckney@icloud.com>
2016-12-17 01:06:39 -1000

    Write code for AES, still need to write subroutines.

--- Sources/AES128.swift
+++ Sources/AES128.swift
@@ -6,26 +6,44 @@
 //
 //
 
-func keySchedule(_ key: ByteString) -> [ByteString] {
-    return [key] // TODO: Implement me!
+private let irreduciblePolynomial: Byte = 0x1b
+private let field = ByteField(irreduciblePolynomial: irreduciblePolynomial)
+private let stateFamily = MatrixFamily(ring: field)
+
+typealias Word = (Byte, Byte, Byte, Byte)
+
+func keySchedule(_ key: ByteString) -> [Word] {
+    return [(key[0], key[0], key[0], key[0])] // TODO: Implement me!
 }
 
+
+
 func AES128(_ key: ByteString, _ plainText: ByteString) -> ByteString {
+    let Nb = 4
+    let Nr = 10
+    
     // Verify we have correctly sized key / plaintext
     precondition(key.count == 16)
     precondition(plainText.count == 16)
     
-    let irreduciblePolynomial: Byte = 0x1b
-    let field = ByteField(irreduciblePolynomial: irreduciblePolynomial)
-    let stateFamily = MatrixFamily(ring: field)
+
     
-    let keys = keySchedule(key) // count == 11
+    let keys = keySchedule(key) // count == Nb*(Nr+1)
     
     var state = stateFamily.dataMatrix(width: 4, data: plainText).transpose()
     
+    state = addRoundKey(state, keys[0...(Nb-1)])
     
+    for round in 1...(Nr-1) {
+        state = subBytes(state)
+        state = shiftRows(state)
+        state = mixColumns(state)
+        state = addRoundKey(state, keys[(round*Nb)...((round+1)*Nb-1)])
+    }
     
-    
+    state = subBytes(state)
+    state = shiftRows(state)
+    state = addRoundKey(state, keys[(Nr*Nb)...((Nr+1)*Nb-1)])
     
     
     return state.transpose().data
